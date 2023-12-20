ef0a74a6e862d584087954645a98076bc64dc08f
Swarnim Bajra <bajraswarnim@gmail.com>
2023-12-20 11:07:35 +0545

    Feat: update bash script && changed to mvc architect

---
+++ headers/database.h
@@ -0,0 +1,8 @@
+// database.h
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+int dbConnection();
+
+#endif // DATABASE_H
--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
-#include <iostream> 
-#include <sqlite3.h> 
-  
-int main(int argc, char** argv) 
-{ 
-    sqlite3* DB; 
-    int exit = 0; 
-    exit = sqlite3_open("bazarmilo.db", &DB); 
-  
-    if (exit) { 
-        std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl; 
-        return (-1); 
-    } 
-    else
-        std::cout << "Opened Database Successfully!" << std::endl; 
-    sqlite3_close(DB); 
-    return (0); 
-} 
+#include <iostream>
+#include "./headers/database.h"
+
+int main(int argc, char **argv)
+{
+    int result = dbConnection();
+    if (result >= 0)
+    {
+        std::cout << "conneced" << std::endl;
+    }
+    return 0;
+}
